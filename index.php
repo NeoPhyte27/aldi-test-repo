fb1a1d7d215edff0d3891bff0eeb2836f94a220e
NeoPhyte27 <aldijinkpro27@gmail.com>
2024-11-03 21:34:13 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, eveniet fugit consectetur saepe, rem laudantium, deserunt officiis iure eaque ullam dolor! Magnam cum rem fugit voluptate ducimus officia quia natus.</p>
 </body>
 </html>
