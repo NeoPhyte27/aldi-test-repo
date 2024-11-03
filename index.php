b6888675cadcb32e1822973691467a8555a3328a
NeoPhyte27 <aldijinkpro27@gmail.com>
2024-11-03 21:48:55 +0700

    menambahkan ul li di index.php

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, eveniet fugit consectetur saepe, rem laudantium, deserunt officiis iure eaque ullam dolor! Magnam cum rem fugit voluptate ducimus officia quia natus.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
